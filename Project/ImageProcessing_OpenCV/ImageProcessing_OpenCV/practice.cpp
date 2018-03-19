feca708a02ec0e8014f3e99136d836b4af6c2b78
MyunghoLee <12audgh@naver.com>
2018-03-19 15:18:40 +0900

    화소 점 처리(논리연산)

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -16,83 +16,56 @@ int main()
 	int i, j;
 
 	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE); // cvLoadImage("주소", 컬러로 로드): 이미지 불러오는 함수
-	IplImage* sumImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
-	IplImage* subImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
-	IplImage* mulImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
-	IplImage* divImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
-	
-	CvScalar pixelValue, temp;
+	IplImage* mask = cvLoadImage("mask.png", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* andImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
+	IplImage* orImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
+	IplImage* xorImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
+
+	CvScalar pixelValue;
+
+	unsigned char temp1, temp2;
 
 	for (i = 0; i < inputImage->height; i++) {
 		for (j = 0; j < inputImage->width; j++) {
 			pixelValue = cvGet2D(inputImage, i, j);
-			// 덧셈 연산
-			if (pixelValue.val[0] + SUM_CONSTANT >= 255) {
-				temp.val[0] = 255;
-				cvSet2D(sumImage, i, j, temp);
-			}
-			else {
-				temp.val[0] = pixelValue.val[0] + SUM_CONSTANT;
-				cvSet2D(sumImage, i, j, temp);
-			}
-			// 뺄셈 연산
-			if (pixelValue.val[0] - SUB_CONSTANT < 0)
-			{
-				temp.val[0] = 0;
-				cvSet2D(subImage, i, j, temp);
-			}
-			else {
-				temp.val[0] = pixelValue.val[0] - SUB_CONSTANT;
-				cvSet2D(subImage, i, j, temp);
-			}
-			// 곱셈 연산
-			if (pixelValue.val[0] + MUL_CONSTANT >= 0)
-			{
-				temp.val[0] = 255;
-				cvSet2D(mulImage, i, j, temp);
-			}
-			else {
-				temp.val[0] = pixelValue.val[0] * MUL_CONSTANT;
-				cvSet2D(mulImage, i, j, temp);
-			}
-			// 나눗셈 연산
-			if (pixelValue.val[0] / DIV_CONSTANT < 0)
-			{
-				temp.val[0] = 0;
-				cvSet2D(divImage, i, j, temp);
-			}
-			else {
-				temp.val[0] = pixelValue.val[0] - DIV_CONSTANT;
-				cvSet2D(divImage, i, j, temp);
-			}
+			temp1 = (unsigned char)pixelValue.val[0];
+
+			pixelValue = cvGet2D(mask, i, j);
+			temp2 = (unsigned char)pixelValue.val[0];
+
+			pixelValue.val[0] = (double)(temp1 & temp2);
+			cvSet2D(andImage, i, j, pixelValue);
+
+			pixelValue.val[0] = (double)(temp1 | temp2);
+			cvSet2D(orImage, i, j, pixelValue);
+
+			pixelValue.val[0] = (double)(temp1 ^ temp2);
+			cvSet2D(xorImage, i, j, pixelValue);
 		}
 	}
 
 	cvNamedWindow("input image", CV_WINDOW_AUTOSIZE); // cvNamedWindow("창 이름", 오토사이즈) 창 만드는 함수
-	cvNamedWindow("sum image", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("sub image", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("mul image", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("div image", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("And", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("Or", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("Xor", CV_WINDOW_AUTOSIZE);
 	
 	cvShowImage("input image", inputImage); // 이미지 보이게 하는 함수
-	cvShowImage("sum image", sumImage); // 이미지 보이게 하는 함수
-	cvShowImage("sub image", subImage);
-	cvShowImage("mul image", mulImage);
-	cvShowImage("div image", divImage);
-
+	cvShowImage("And", andImage);
+	cvShowImage("Or", orImage);
+	cvShowImage("Xor", xorImage);
+	
 	cvWaitKey(); // 이 함수를 안넣으면 이미지가 불러와졌다가 바로 꺼짐(괄호 안에 특수한 키를 넣으면 그 키를 눌렀을 때 꺼짐, 비어있으면 아무키나 눌렀을 때 꺼진다.)
 	
 	cvDestroyWindow("input image"); // 창 끄기
-	cvDestroyWindow("sum image");
-	cvDestroyWindow("sub image");
-	cvDestroyWindow("mul image");
-	cvDestroyWindow("div image");
-
+	cvDestroyWindow("And");
+	cvDestroyWindow("Or");
+	cvDestroyWindow("Xor");
+	
 	cvReleaseImage(&inputImage); // 이미지 해제
-	cvReleaseImage(&sumImage);
-	cvReleaseImage(&subImage);
-	cvReleaseImage(&mulImage);
-	cvReleaseImage(&divImage);
+	cvReleaseImage(&mask);
+	cvReleaseImage(&andImage);
+	cvReleaseImage(&orImage);
+	cvReleaseImage(&xorImage);
 
 	return 0;
 }
