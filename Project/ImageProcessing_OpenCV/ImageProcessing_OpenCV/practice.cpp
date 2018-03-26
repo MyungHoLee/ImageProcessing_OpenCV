98c5da2b1c2f09807203e7365d842576097908cc
MyunghoLee <12audgh@naver.com>
2018-03-26 16:09:13 +0900

    엔드-인 탐색

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -5,135 +5,100 @@
 #define HIGH 255
 #define HISTOGRAM_SIZE 256
 
+#define ENDIN_LOW 30
+#define ENDIN_HIGH 225
+
 /*
-	히스토그램은 시험에도 나오므로 잘 공부해두어야 한다.
+* 히스토그램을 이용한 화소 점처리
 */
+void Show_histogram(IplImage* img,const char* imgWindowNmae,const char* histogramWindowName);
+
 int main()
 {
-	int i, j, value;
+	int i, j;
 
-	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_COLOR); // cvLoadImage("주소", 컬러로 로드): 이미지 불러오는 함수
-	IplImage* histogramImage_R = cvCreateImage(cvSize(HISTOGRAM_SIZE, HISTOGRAM_SIZE + 20), IPL_DEPTH_8U, 3);
-	IplImage* histogramImage_G = cvCreateImage(cvSize(HISTOGRAM_SIZE, HISTOGRAM_SIZE + 20), IPL_DEPTH_8U, 3);
-	IplImage* histogramImage_B = cvCreateImage(cvSize(HISTOGRAM_SIZE, HISTOGRAM_SIZE + 20), IPL_DEPTH_8U, 3);
-	
+	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE); // cvLoadImage("주소", 컬러로 로드): 이미지 불러오는 함수
+	IplImage* endin_Image = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
+
 	CvScalar temp;
 
-	double HIST_R[HISTOGRAM_SIZE], HIST_G[HISTOGRAM_SIZE], HIST_B[HISTOGRAM_SIZE];
-	unsigned char scale_HIST[HISTOGRAM_SIZE];
-	double MAX;
-
-	//히스토그램 배열 초기화
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		HIST_R[i] = LOW;
-		HIST_G[i] = LOW;
-		HIST_B[i] = LOW;
-	}
-
-	//빈도수 조사
 	for (i = 0; i < inputImage->height; i++) {
 		for (j = 0; j < inputImage->width; j++) {
 			temp = cvGet2D(inputImage, i, j);
-			
-			HIST_R[(int)temp.val[2]]++;
-			HIST_G[(int)temp.val[1]]++;
-			HIST_B[(int)temp.val[0]]++;
+			if (temp.val[0] >= ENDIN_HIGH) {
+				cvSet2D(endin_Image, i, j, cvScalar(255));
+			}
+			else if (temp.val[0] <= ENDIN_LOW) {
+				cvSet2D(endin_Image, i, j, cvScalar(0));
+			}
+			else {
+				cvSet2D(endin_Image, i, j, cvScalar((temp.val[0] - ENDIN_LOW) * HIGH / (ENDIN_HIGH - ENDIN_LOW)));
+			}
 		}
 	}
 
-	//R 히스토그램 그리기
-	MAX = HIST_R[0];
+	Show_histogram(inputImage, "input image", "input histogram");
+	Show_histogram(endin_Image, "EndIn image", "EndIn histogram");
 
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		if (HIST_R[i] > MAX)
-			MAX = HIST_R[i];
-	}
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		scale_HIST[i] = (unsigned char)((HIST_R[i] * HIGH) / MAX);
-	}
-
-	cvSet(histogramImage_R, cvScalar(255, 255, 255));
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		for (j = 0; j < scale_HIST[i]; j++) {
-			cvSet2D(histogramImage_R, HISTOGRAM_SIZE - j - 1, i, cvScalar(0));
-		}
-	}
-
-	for (i = HISTOGRAM_SIZE + 5; i < HISTOGRAM_SIZE + 20; i++) {
-		for (j = 0; j < HISTOGRAM_SIZE; j++) {
-			cvSet2D(histogramImage_R, i, j, cvScalar(0, 0, j));
-		}
-	}
-
-	//G 히스토그램 그리기
-	MAX = HIST_G[0];
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		if (HIST_G[i] > MAX)
-			MAX = HIST_G[i];
-	}
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		scale_HIST[i] = (unsigned char)((HIST_G[i] * HIGH) / MAX);
-	}
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		scale_HIST[i] = (unsigned char)((HIST_G[i] * HIGH) / MAX);
-	}
-	cvSet(histogramImage_G, cvScalar(255, 255, 255));
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		for (j = 0; j < scale_HIST[i]; j++) {
-			cvSet2D(histogramImage_G, HISTOGRAM_SIZE - j - 1, i, cvScalar(0));
-		}
-	}
-
-	for (i = HISTOGRAM_SIZE + 5; i < HISTOGRAM_SIZE + 20; i++) {
-		for (j = 0; j < HISTOGRAM_SIZE; j++) {
-			cvSet2D(histogramImage_G, i, j, cvScalar(0, j, 0));
-		}
-	}
-
-	//B 히스토그램 그리기
-	MAX = HIST_B[0];
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		if (HIST_B[i] > MAX)
-			MAX = HIST_B[i];
-	}
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		scale_HIST[i] = (unsigned char)((HIST_B[i] * HIGH) / MAX);
-	}
-	cvSet(histogramImage_B, cvScalar(255, 255, 255));
-
-	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		for (j = 0; j < scale_HIST[i]; j++) {
-			cvSet2D(histogramImage_B, HISTOGRAM_SIZE - j - 1, i, cvScalar(0));
-		}
-	}
-
-	for (i = HISTOGRAM_SIZE + 5; i < HISTOGRAM_SIZE + 20; i++) {
-		for (j = 0; j < HISTOGRAM_SIZE; j++) {
-			cvSet2D(histogramImage_B, i, j, cvScalar(j, 0, 0));
-		}
-	}
-	
-	cvShowImage("input image", inputImage); // 이미지 보이게 하는 함수
-	cvShowImage("histoImage_R image", histogramImage_R);
-	cvShowImage("histoImage_G image", histogramImage_G);
-	cvShowImage("histoImage_B image", histogramImage_B);
-	
 	cvWaitKey(); // 이 함수를 안넣으면 이미지가 불러와졌다가 바로 꺼짐(괄호 안에 특수한 키를 넣으면 그 키를 눌렀을 때 꺼짐, 비어있으면 아무키나 눌렀을 때 꺼진다.)
-	
 	cvDestroyAllWindows();
-
-	cvReleaseImage(&histogramImage_R);
-	cvReleaseImage(&histogramImage_G);
-	cvReleaseImage(&histogramImage_B);
 	cvReleaseImage(&inputImage); // 이미지 해제
+	cvReleaseImage(&inputImage);
 
 	return 0;
+}
+
+void Show_histogram(IplImage* img,const char* imgWindowName,const char* histogramWindowName)
+{
+	int i, j, value;
+	IplImage* inputImage = img;
+	IplImage* histogramImage = cvCreateImage(cvSize(HISTOGRAM_SIZE, HISTOGRAM_SIZE + 20), IPL_DEPTH_8U, 1);
+
+	CvScalar temp;
+
+	double HIST[HISTOGRAM_SIZE];
+	unsigned char scale_HIST[HISTOGRAM_SIZE];
+	double MAX, MIN, DIF;
+
+	// 히스토그램 배열 초기화
+	for (i = 0; i < HISTOGRAM_SIZE; i++) HIST[i] = LOW;
+
+	// 빈도수 조사
+	for (i = 0; i < inputImage->height; i++) {
+		for (j = 0; j < inputImage->width; j++) {
+			temp = cvGet2D(inputImage, i, j);
+			value = (int)temp.val[0];
+			HIST[value]++;
+		}
+	}
+
+	// 정규화
+	MAX = HIST[0];
+	MIN = HIST[0];
+
+	for (i = 0; i < HISTOGRAM_SIZE; i++) {
+		if (HIST[i] > MAX) MAX = HIST[i];
+	}
+
+	for (i = 0; i < HISTOGRAM_SIZE; i++) scale_HIST[i] = (unsigned char)((HIST[i] * HIGH) / MAX);
+
+	cvSet(histogramImage, cvScalar(255));
+
+	//히스토그램의 값은 검은색으로 출력
+	for (i = 0; i < HISTOGRAM_SIZE; i++) {
+		for (j = 0; j < scale_HIST[i]; j++) {
+			cvSet2D(histogramImage, HISTOGRAM_SIZE - j - 1, i, cvScalar(0));
+		}
+	}
+
+	//아래 부분에 히스토그램의 색을 표시
+	for (i = HISTOGRAM_SIZE + 5; i < HISTOGRAM_SIZE + 20; i++) {
+		for (j = 0; j < HISTOGRAM_SIZE; j++) {
+			cvSet2D(histogramImage, i, j, cvScalar(j));
+		}
+	}
+
+	cvShowImage(imgWindowName, inputImage);
+	cvShowImage(histogramWindowName, histogramImage);
+
 }
