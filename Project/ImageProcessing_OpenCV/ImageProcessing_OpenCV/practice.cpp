98501001eaef9f5fade47492107ce9128c880847
MyunghoLee <12audgh@naver.com>
2018-04-02 15:46:42 +0900

    히스토그램 명세화

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -6,57 +6,90 @@
 #define HISTOGRAM_SIZE 256
 
 /*
-* 히스토그램 평활화
-단계정도는 알아두기
-어떤 수식이 어떤 수식인지 알아두기
+* 히스토그램 명세화
 */
 void Show_histogram(IplImage* img,const char* imgWindowNmae,const char* histogramWindowName);
 
 int main()
 {
-	int i, j, value;
-
 	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE); // cvLoadImage("주소", 컬러로 로드): 이미지 불러오는 함수
-	IplImage* equalImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
+	IplImage* targetImage = cvLoadImage("Jeju.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* specImage = cvCreateImage(cvGetSize(inputImage), inputImage->depth, inputImage->nChannels);
+
+	int i, j, value;
+	double in_SUM = 0.0;
+	double tar_SUM = 0.0;
 
 	CvScalar temp;
 
-	double HIST[HISTOGRAM_SIZE];
-	double sum_of_HIST[HISTOGRAM_SIZE];
-	double SUM = 0.0;
+	double inputHist[HISTOGRAM_SIZE];
+	int inputEqHist[HISTOGRAM_SIZE];
+	double targetHist[HISTOGRAM_SIZE];
+	int targetEqHist[HISTOGRAM_SIZE];
+	int RevHist[HISTOGRAM_SIZE];
 
 	// 초기화
-	for (i = 0; i < HISTOGRAM_SIZE; i++) HIST[i] = LOW;
+	for (i = 0; i < HISTOGRAM_SIZE; i++) {
+		inputHist[i] = LOW;
+		targetHist[i] = LOW;
+	}
 
-	// 1단계 빈도수
+	// 히스토그램 만들기
+	// 인풋
 	for (i = 0; i < inputImage->height; i++) {
 		for (j = 0; j < inputImage->width; j++) {
 			temp = cvGet2D(inputImage, i, j);
 			value = (int)temp.val[0];
-			HIST[value]++;
+			inputHist[value]++;
 		}
 	}
 
-	// 2단계 누적합
+	// 타겟
+	for (i = 0; i < targetImage->height; i++) {
+		for (j = 0; j < targetImage->width; j++) {
+			temp = cvGet2D(targetImage, i, j);
+			value = (int)temp.val[0];
+			targetHist[value]++;
+		}
+	}
+	
+
+	// 평활화
 	for (i = 0; i < HISTOGRAM_SIZE; i++) {
-		SUM = SUM + HIST[i];
-		sum_of_HIST[i] = SUM;
+		in_SUM = in_SUM + inputHist[i];
+		inputEqHist[i] = (int)(in_SUM * HIGH / (inputImage->height * inputImage->width));
+
+		tar_SUM = tar_SUM + targetHist[i];
+		targetEqHist[i] = (int)(tar_SUM * HIGH / (targetImage->height * targetImage->width));
 	}
 
-	// 3단계 정규화 누적합
+	// 역평활화 테이블
+	value = targetEqHist[0];
+	for (i = 0; i < HISTOGRAM_SIZE; i++) {
+		for (j = 0; j < HISTOGRAM_SIZE; j++) {
+			if (i == targetEqHist[j]) {
+				value = j;
+				break;
+			}
+		}
+		RevHist[i] = value;
+	}
+
+	// 명세화
 	for (i = 0; i < inputImage->height; i++) {
 		for (j = 0; j < inputImage->width; j++) {
 			temp = cvGet2D(inputImage, i, j);
-			cvSet2D(equalImage, i, j, cvScalar(sum_of_HIST[(int)temp.val[0]] * HIGH / (inputImage->height * inputImage->width)));
+			cvSet2D(specImage, i, j, cvScalar(RevHist[(int)temp.val[0]]));
 		}
 	}
 
 	Show_histogram(inputImage, "input image", "input histogram");
-	Show_histogram(equalImage, "Equal image", "Equal histogram");
+	Show_histogram(targetImage, "target image", "target histogram");
+	Show_histogram(specImage, "spec image", "spec histogram");
 
 	cvWaitKey(); // 이 함수를 안넣으면 이미지가 불러와졌다가 바로 꺼짐(괄호 안에 특수한 키를 넣으면 그 키를 눌렀을 때 꺼짐, 비어있으면 아무키나 눌렀을 때 꺼진다.)
 	cvDestroyAllWindows();
-	cvReleaseImage(&equalImage); // 이미지 해제
+	cvReleaseImage(&targetImage); // 이미지 해제
 	cvReleaseImage(&inputImage);
 
 	return 0;
