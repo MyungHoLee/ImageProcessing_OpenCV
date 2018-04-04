79c12c6c43dd6b122bc3e9ad29ca116c4c53fb33
MyunghoLee <12audgh@naver.com>
2018-04-04 15:24:04 +0900

    엠보싱 처리_코드수정

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -2,30 +2,26 @@
 #include <opencv/highgui.h>
 
 /*
-	가우시안 필터링 처리
+* 엠보싱 처리
+* 마스크 크기를 바꾸고 tempImage의 크기가 몇으로 바뀌는지 시험 출제 가능성 높음
 */
 IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]);
 
 int main()
 {
 	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE); // cvLoadImage("주소", 컬러로 로드): 이미지 불러오는 함수
-	IplImage* ResultImage_1 = NULL;
-	IplImage* ResultImage_2 = NULL;
+	IplImage* ResultImage = NULL;
 
-	double SharpeningMask_1[3][3] = { {-1., -1., -1.}, {-1.,9.,-1.},{-1.,-1.,-1.} };
-	double SharpeningMask_2[3][3] = { {0., -1., 0.}, {-1., 5., -1.}, {0., -1., 0.} };
+	double EmboMask[3][3] = { { -1., 0., 0. },{ 0.,0.,0. },{ 0.,0.,1. } };
 
-	ResultImage_1 = ConvolutionProcess(inputImage, SharpeningMask_1);
-	ResultImage_2 = ConvolutionProcess(inputImage, SharpeningMask_2);
+	ResultImage = ConvolutionProcess(inputImage, EmboMask);
 
 	cvShowImage("input Image", inputImage);
-	cvShowImage("Result Image 1", ResultImage_1);
-	cvShowImage("Result Image 2", ResultImage_2);
+	cvShowImage("Result Image", ResultImage);
 
 	cvWaitKey(); // 이 함수를 안넣으면 이미지가 불러와졌다가 바로 꺼짐(괄호 안에 특수한 키를 넣으면 그 키를 눌렀을 때 꺼짐, 비어있으면 아무키나 눌렀을 때 꺼진다.)
 	cvDestroyAllWindows();
-	cvReleaseImage(&ResultImage_1);
-	cvReleaseImage(&ResultImage_2);
+	cvReleaseImage(&ResultImage);
 	cvReleaseImage(&inputImage);
 
 	return 0;
@@ -57,11 +53,11 @@ IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]) {
 					Sum += Mask[n][m] * tempScalar.val[0];
 				}
 			}
-			cvSet2D(outputImage, i, j, cvScalar(Sum)); // 결과값을 아웃풋 이미지에 넣음
+			cvSet2D(outputImage, i, j, cvScalar(Sum + 128)); // 결과값을 아웃풋 이미지에 넣음
 			Sum = 0.0;
 		}
 	}
-	
+
 	cvReleaseImage(&tempinputImage);
 
 	return outputImage;
