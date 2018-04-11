639de29be5cbab941a670fa0fca6817f1e4a576e
MyunghoLee <12audgh@naver.com>
2018-04-11 16:08:33 +0900

    양선형 화소 보간법

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -1,48 +1,53 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
-#define ZOOM_RATE 2.0 // 영상 확대 비율
+#define SUB_SAMPLING_RATE 2 // 축소 비율
 
+void Swap(double *a, double *b); // 데이터 교환
+void BubbleSort(double *A, int MAX); // 데이터 정렬
 /*
 * 양선형 화소 보간법
 */
 
 int main()
 {
-	int i, j, i_H, i_W;
-	double r_H, r_W, s_H, s_W;
-	CvScalar C1, C2, C3, C4, newValue;
+	int i, j, n, m, k, index = 0;
+	double *Mask, *OutputValue;
+	CvScalar tempScalar;
 
-	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE); // cvLoadImage("주소", 컬러로 로드): 이미지 불러오는 함수
-	IplImage* outputImage = cvCreateImage(cvSize(inputImage->width * ZOOM_RATE, inputImage->height * ZOOM_RATE), 8, 1);
+	Mask = new double[SUB_SAMPLING_RATE * SUB_SAMPLING_RATE]; // 마스크 크기
 
-	for (i = 0; i < outputImage->height; i++) {
+	IplImage *inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage *outputImage = cvCreateImage(cvSize((inputImage->width + 1)/ SUB_SAMPLING_RATE, (inputImage->height
+		+1) / SUB_SAMPLING_RATE), 8, 1);
+	IplImage *tempImage = cvCreateImage(cvSize(inputImage->width + 1, inputImage->height + 1), 8, 1);
+
+	OutputValue = new double[(inputImage->width + 1) / SUB_SAMPLING_RATE * (inputImage->height + 1) / SUB_SAMPLING_RATE];
+	
+	cvSetZero(tempImage);
+
+	for (i = 0; i < inputImage->height; i++) { // 인풋이미지 temp에 옮김
+		for (j = 0; j < inputImage->width; j++) {
+			cvSet2D(tempImage, i, j, cvGet2D(inputImage, i, j));
+		}
+	}
+
+	for (i = 0; i < inputImage->height; i = i + SUB_SAMPLING_RATE) {
+		for (j = 0; j < inputImage->width; j = j + SUB_SAMPLING_RATE) {
+			for (n = 0; n < SUB_SAMPLING_RATE; n++) {
+				for (m = 0; m < SUB_SAMPLING_RATE; m++) {
+					tempScalar = cvGet2D(tempImage, i + n, j + m);
+					Mask[n*SUB_SAMPLING_RATE + m] = tempScalar.val[0]; // 마스크 범위의 화소값들 배열을 만듬
+				}
+			}
+			BubbleSort(Mask, SUB_SAMPLING_RATE * SUB_SAMPLING_RATE); // 마스크 배열 값들 정렬
+			OutputValue[index++] = Mask[(int)(SUB_SAMPLING_RATE * SUB_SAMPLING_RATE / 2)]; // OutputValue 행렬에 중간값들 행렬
+		}
+	}
+
+	for (i = 0; i < outputImage->height; i++) { //OutputValue 행렬 값들 outputImage에 입력
 		for (j = 0; j < outputImage->width; j++) {
-
-			r_H = i / ZOOM_RATE; // 목적영상의 원시영상에서 정확한 좌표
-			r_W = j / ZOOM_RATE;
-
-			i_H = (int)r_W; // 소수점 버림
-			i_W = (int)r_W;
-
-			s_H = r_H - i_H;
-			s_W = r_W - i_W;
-
-			if (i_H >= (inputImage->height - 1) || i_W >= (inputImage->width - 1)) {
-				cvSet2D(outputImage, i, j, cvScalar(0));
-			}
-			else {
-				C1 = cvGet2D(inputImage, i_H, i_W);
-				C2 = cvGet2D(inputImage, i_H, i_W + 1);
-				C3 = cvGet2D(inputImage, i_H + 1, i_W + 1);
-				C4 = cvGet2D(inputImage, i_H + 1, i_W);
-				newValue.val[0] = (C1.val[0] * (1 - s_W)*(1 - s_H) + C2.val[0] * s_W * (1 - s_H) +
-					C3.val[0] * s_W * s_H + C4.val[0] * (1 - s_W)* s_H);
-				cvSet2D(outputImage, i, j, newValue);
-			}
-
-			cvSet2D(outputImage, i, j, cvGet2D(inputImage, i / ZOOM_RATE, j / ZOOM_RATE));
-			// 가장 인접환 화소 입력
+			cvSet2D(outputImage, i, j, cvScalar(OutputValue[i*outputImage->width + j]));
 		}
 	}
 
@@ -58,38 +63,23 @@ int main()
 	return 0;
 }
 
-// 컨볼루션 연산 함수
-IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]) {
-	IplImage* tempinputImage = cvCreateImage(cvSize(inputImage->width + 2, inputImage->height + 2), IPL_DEPTH_8U, 1); // 인풋 이미지보다 2씩 크게 선언
-	IplImage* outputImage = cvCreateImage(cvGetSize(inputImage), IPL_DEPTH_8U, 1);
+void Swap(double *a, double *b)
+{
+	double temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-	int i, j, n, m;
-	double Sum = 0.0;
-	CvScalar tempScalar;
+void BubbleSort(double *A, int MAX)
+{
+	int i, j;
 
-	cvSetZero(tempinputImage); // 경계처리는 0 삽입을 이용, temp이미지 검은색 채움
-
-	for (i = 0; i < inputImage->height; i++) { // temp이미지에 인풋 이미지 복사
-		for (j = 0; j < inputImage->width; j++) {
-			cvSet2D(tempinputImage, i + 1, j + 1, cvGet2D(inputImage, i, j));
-		}
-	}
-
-	for (i = 0; i < inputImage->height; i++) // 컨벌루션 연산
-	{
-		for (j = 0; j < inputImage->width; j++) {
-			for (n = 0; n < 3; n++) {
-				for (m = 0; m < 3; m++) { // 마스크와 연산하면서 Sum에 누적
-					tempScalar = cvGet2D(tempinputImage, i + n, j + m);
-					Sum += Mask[n][m] * tempScalar.val[0];
-				}
+	for (i = 0; i < MAX; i++) {
+		for (j = 0; j < MAX - 1; j++) {
+			if (A[j] > A[j + 1]) {
+				Swap(&A[j], &A[j + 1]);
 			}
-			cvSet2D(outputImage, i, j, cvScalar(Sum)); // 결과값을 아웃풋 이미지에 넣음
-			Sum = 0.0;
 		}
 	}
-
-	cvReleaseImage(&tempinputImage);
-
-	return outputImage;
 }
