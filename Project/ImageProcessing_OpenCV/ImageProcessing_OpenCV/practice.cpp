c89840392034052a945dcc66c44b08574615374a
MyunghoLee <12audgh@naver.com>
2018-04-16 15:44:40 +0900

    회전

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -1,25 +1,44 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
-#define H_POS 30
-#define W_POS 130
+#define PI 3.141592
+#define DEGREE 45 // 회전 각
 
 /*
-* 이동
+* 회전
 */
 
 int main()
 {
 	IplImage *inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	IplImage *outputImage = cvCreateImage(cvSize(inputImage->width, inputImage->height), inputImage->depth, inputImage->nChannels);
+	IplImage *outputImage = cvCreateImage(cvSize(inputImage->width, inputImage->height),
+		inputImage->depth, inputImage->nChannels);
 
-	int i, j;
+	int i, j, Center_y, Center_x, source_y, source_x;
+	double Radian, cosR, sinR;
+	CvScalar Value;
 
-	cvSetZero(outputImage);
+	Radian = (double)DEGREE * PI / 180.0; // degree 값을 radian으로 변경
 
-	for (i = 0; i < inputImage->height - H_POS; i++) {
-		for (j = 0; j < inputImage->width - W_POS; j++) {
-			cvSet2D(outputImage, i + H_POS, j + W_POS, cvGet2D(inputImage, i, j));
+	cosR = cos(Radian);
+	sinR = sin(Radian);
+
+	//영상의 중심좌표
+	Center_y = inputImage->height / 2;
+	Center_x = inputImage->width / 2;
+
+	for (i = 0; i < inputImage->height; i++) {
+		for (j = 0; j < inputImage->width; j++) {
+			//회전 변환 행렬을 이용하여 새 좌표값 계산
+			source_x = (int)((j - Center_x)*cosR + (i - Center_y)*sinR + Center_x);
+			source_y = (int)(-(j - Center_x)*sinR + (i - Center_y)*cosR + Center_y);
+
+			//좌표가 영상범위 넘어갔을때 0 처리
+			if (source_x < 0 || source_y < 0 || source_y >= inputImage->height || source_x >= inputImage->width)
+				Value.val[0] = 0;
+			else
+				Value = cvGet2D(inputImage, source_y, source_x);
+			cvSet2D(outputImage, i, j, Value);
 		}
 	}
 
