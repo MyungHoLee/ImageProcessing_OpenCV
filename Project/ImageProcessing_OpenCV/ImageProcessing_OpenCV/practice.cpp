afe73846c93280c1de0576db014a0e482347caf4
MyunghoLee <12audgh@naver.com>
2018-04-30 15:35:35 +0900

    프레임 처리

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -1,61 +1,38 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
-#define PI 3.141592
-#define DEGREE 300 // 회전 각
-
-/*
-* 회전
-*/
-
 int main()
 {
-	IplImage *inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	IplImage *outputImage;
+	int i, j;
+	CvScalar inputValue1, inputValue2, outputValue;
 
-	int i, j, in_Center_y, in_Center_x, source_y, source_x, out_w, out_h, out_Center_y, out_Center_x;
-	double Radian, cosR, sinR, Radian90;
-	CvScalar Value;
+	IplImage *inputImage1 = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage *inputImage2 = cvLoadImage("Penguin.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage *outputImage = cvCreateImage(cvSize(inputImage1->width, inputImage1->height),8,1);
 
-	Radian = (double)DEGREE * PI / 180.0; // degree 값을 radian으로 변경
-	Radian90 = (double)90 * PI / 180.0;
-	//Out put Image size
-	out_w = abs(inputImage->height * cos(Radian90 - Radian)) + abs(inputImage->width * cos(Radian));
-	out_h = abs(inputImage->height * cos(Radian)) + abs(inputImage->width * cos(Radian90 - Radian));
+	for (i = 0; i < inputImage1->height; i++) {
+		for (j = 0; j < inputImage1->width; j++) {
+			//각 이미지에서 같은 좌표 값을 불러옴
+			inputValue1 = cvGet2D(inputImage1, i, j);
+			inputValue2 = cvGet2D(inputImage2, i, j);
+			//각각 1/2씩 합
+			outputValue.val[0] = inputValue1.val[0] / 2 + inputValue2.val[0] / 2;
 
-	outputImage = cvCreateImage(cvSize(out_w, out_h), inputImage->depth, inputImage->nChannels);
-
-	cosR = cos(Radian);
-	sinR = sin(Radian);
-
-	//영상의 중심좌표
-	out_Center_y = out_h / 2;
-	out_Center_x = out_w / 2;
-	in_Center_y = inputImage->height / 2;
-	in_Center_x = inputImage->width / 2;
-
-	for (i = 0; i < out_h; i++) {
-		for (j = 0; j < out_w; j++) {
-			source_x = (int)((j - out_Center_x)*cosR + (i - out_Center_y)*sinR + in_Center_x);
-			source_y = (int)(-(j - out_Center_x)*sinR + (i - out_Center_y)*cosR + in_Center_y);
-			
-			//좌표가 영상범위 넘어갔을때 0 처리
-			if (source_x < 0 || source_y < 0 || source_y >= inputImage->height || source_x >= inputImage->width)
-				Value.val[0] = 0;
-			else
-				Value = cvGet2D(inputImage, source_y, source_x);
-			cvSet2D(outputImage, i, j, Value);
+			cvSet2D(outputImage, i, j, outputValue);
 		}
 	}
 
-	cvShowImage("input Image", inputImage);
+
+	cvShowImage("Input Image1", inputImage1);
+	cvShowImage("Input Image2", inputImage2);
 	cvShowImage("Output Image", outputImage);
 
 	cvWaitKey(); // 이 함수를 안넣으면 이미지가 불러와졌다가 바로 꺼짐(괄호 안에 특수한 키를 넣으면 그 키를 눌렀을 때 꺼짐, 비어있으면 아무키나 눌렀을 때 꺼진다.)
 	
 	cvDestroyAllWindows();
+	cvReleaseImage(&inputImage1);
+	cvReleaseImage(&inputImage2);
 	cvReleaseImage(&outputImage);
-	cvReleaseImage(&inputImage);
-
+	
 	return 0;
 }
