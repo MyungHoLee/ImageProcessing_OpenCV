c363a1071c3b2183303bc02f666fd9d85f1bec78
MyunghoLee <12audgh@naver.com>
2018-05-09 15:57:03 +0900

    이진화 영상 형태학 처리

--- Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
+++ Project/ImageProcessing_OpenCV/ImageProcessing_OpenCV/practice.cpp
@@ -1,52 +1,77 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+#define THRESHOLD 130
+
 int main()
 {
-	int i, j;
-	CvScalar tempValue01, tempValue02, resultValue;
-	bool isBackGroung = false;
+	int i, j, n, m, Dilation_Sum = 0, Erosion_Sum = 0;
+	CvScalar tempValue;
+	//침식마스크
+	double Erosion_Mask[3][3] = { {255.,255., 255.},{ 255., 255., 255.}, { 255., 255., 255.} };
+	//팽창 마스크
+	double Dilation_Mask[3][3] = { {0.,0., 0.}, { 0., 0., 0.}, { 0., 0., 0.} };
 
-	CvCapture *capture = cvCreateFileCapture("test.avi");// 동영상 연결
-	IplImage *frame;
+	IplImage *inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage *binaryImage = cvCreateImage(cvGetSize(inputImage), 8, 1);
+	IplImage *tempImage = cvCreateImage(cvSize(inputImage->width + 2, inputImage->height + 2), 8, 1);
+	IplImage *ErosionImage = cvCreateImage(cvGetSize(inputImage), 8, 1);
+	IplImage *DilationImage = cvCreateImage(cvGetSize(inputImage), 8, 1);
 
-	IplImage *grayFrame = cvCreateImage(cvSize(320, 240), 8, 1);
-	IplImage *backGroundImage = cvCreateImage(cvGetSize(grayFrame), 8, 1);
-	IplImage *resultImage = cvCreateImage(cvGetSize(grayFrame), 8, 1);
-
-	while (1) {
-		frame = cvQueryFrame(capture); // 동영상의 프레임을 복사
-		if (!frame) break;
-		cvShowImage("Source", frame);
-
-		cvCvtColor(frame, grayFrame, CV_RGB2GRAY);// 컬러영상을 흑백으로 변환
-		cvShowImage("Gray Scale", grayFrame);
-
-		if (isBackGroung != true) {
-			cvCopy(grayFrame, backGroundImage); // 배경영상을 추출.
-			isBackGroung = true;
+	//이진화
+	for (i = 0; i < inputImage->height; i++) {
+		for (j = 0; j < inputImage->width; j++) {
+			tempValue = cvGet2D(inputImage, i, j);
+			if (tempValue.val[0] > THRESHOLD)
+				cvSet2D(binaryImage, i, j, cvScalar(255));
+			else
+				cvSet2D(binaryImage, i, j, cvScalar(0));
 		}
-		cvShowImage("Back Ground Image", backGroundImage);
-
-		for (i = 0; i < grayFrame->height; i++) {
-			for (j = 0; j < grayFrame ->width; j++) {
-				tempValue01 = cvGet2D(grayFrame, i, j);
-				tempValue02 = cvGet2D(backGroundImage, i, j);
-				resultValue.val[0] = abs(tempValue01.val[0] - tempValue02.val[0]);
-				cvSet2D(resultImage, i, j, resultValue);
-			}
-		}
-		cvShowImage("Result Image", resultImage);
-
-		if (cvWaitKey(30) == 27) break; // 30ms동안 프로그램 정지. ESC(27)을 누르지 않으면 루프 반복
 	}
 
+	//패딩만들기
+	for (i = 0; i < binaryImage->height; i++) {
+		for (j = 0; j < binaryImage->width; j++) {
+			cvSet2D(tempImage, i + 1, j + 1, cvGet2D(binaryImage, i, j));
+		}
+	}
 
-	cvReleaseCapture(&capture);
+	//연산
+	for (i = 0; i < binaryImage->height; i++) {
+		for (j = 0; j < binaryImage->width; j++) {
+			for (n = 0; n < 3; n++) {
+				for (m = 0; m < 3; m++) {
+					tempValue = cvGet2D(tempImage, i + n, j + m);
+					if (Erosion_Mask[n][m] == tempValue.val[0])
+						Erosion_Sum += 1;
+					if (Dilation_Mask[n][m] == tempValue.val[0])
+						Dilation_Sum += 1;
+				}
+			}
+			if (Erosion_Sum == 9)
+				cvSet2D(ErosionImage, i, j, cvScalar(255));
+			else
+				cvSet2D(ErosionImage, i, j, cvScalar(0));
+
+			if (Dilation_Sum == 9)
+				cvSet2D(DilationImage, i, j, cvScalar(0));
+			else
+				cvSet2D(DilationImage, i, j, cvScalar(255));
+
+			Erosion_Sum = 0;
+			Dilation_Sum = 0;
+		}
+	}
+
+	cvShowImage("Binary Image", binaryImage);
+	cvShowImage("Erosion Image", ErosionImage);
+	cvShowImage("Dilation Image", DilationImage);
+	cvWaitKey();
+
 	cvDestroyAllWindows();
-	cvReleaseImage(&grayFrame);
-	cvReleaseImage(&backGroundImage);
-	cvReleaseImage(&resultImage);
-	
+	cvReleaseImage(&binaryImage);
+	cvReleaseImage(&ErosionImage);
+	cvReleaseImage(&DilationImage);
+
 	return 0;
 }
